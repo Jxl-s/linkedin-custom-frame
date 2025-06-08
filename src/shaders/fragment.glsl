45387a2b8624d4c4daf8691c23b682a7723afea7
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-06-08 16:58:56 -0400

    yay its working

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,11 @@
 uniform sampler2D uAlphaTexture;
+uniform sampler2D uBackgroundTexture;
 uniform vec3 uRingColor;
 varying vec2 vUv;
 
 void main() {
     float alpha = texture2D(uAlphaTexture, vUv).r;
-    gl_FragColor = vec4(uRingColor, alpha);
+    vec3 color = texture2D(uBackgroundTexture, vUv).rgb;
+    color = mix(color, uRingColor, alpha);
+    gl_FragColor = vec4(color, 1.0);
 }
